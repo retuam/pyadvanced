4d3f7c4aa4116fce20244b752ac06a3c0a412c4c
kirillkravcenko <retuam@gmail.com>
2020-06-14 12:25:42 +0300

    Задание 12

--- homework_12/main.py
+++ homework_12/main.py
@@ -8,7 +8,7 @@ import math
 
 
 bot = TeleBot("1116314719:AAGmn_YjFsl0k8acvd1FBL2f_E_HNe6Cdds")
-
+SIZE = 4096
 
 def get_head(html):
     soup = BeautifulSoup(html, 'lxml')
@@ -37,10 +37,10 @@ def any_text(message):
         bot.send_message(message.chat.id, block)
         fragment = soup.find('div', id='mw-content-text')
         all_text = ''.join(fragment.findAll(text=True))
-        qty = math.ceil(len(all_text) / 4096)
+        qty = math.ceil(len(all_text) / SIZE)
         for i in range(0, qty):
-            min = i * 4096
-            max = (i + 1) * 4096
+            min = i * SIZE
+            max = (i + 1) * SIZE
             bot.send_message(message.chat.id, all_text[min:max])
 
 
