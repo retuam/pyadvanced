0df8c1e1c9cd790a2206fcb459afd6d40efe6cc6
kirillkravcenko <retuam@gmail.com>
2020-06-29 15:14:37 +0300

    Задание 12 delete style

--- course/main.py
+++ course/main.py
@@ -1,5 +1,24 @@
-from course.webshop.bot.main import start_bot
+from course.webshop.bot.main import start_bot, bot
+from flask import Flask, request, abort
+from telebot.types import Update
+from course.webshop.bot.config import WEBHOOK_PATH, WEBHOOK_URL
+
+
+app = Flask(__name__)
+
+
+@app.route(WEBHOOK_PATH, methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = Update.de_json(json_string)
+        bot.process_new_updates([update])
+    else:
+        abort(403)
 
 
 if __name__ == '__main__':
-    start_bot()
+    bot.set_webhook(
+        WEBHOOK_URL,
+        certificate=open('webhook_cert.pem', 'r')
+    )
