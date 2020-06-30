568f67601f53bd1760187601ab589b30c09601d1
kirillkravcenko <retuam@gmail.com>
2020-06-30 19:30:37 +0300

    Задание 12 delete style

--- course/webshop/bot/lookups.py
+++
@@ -1,5 +0,0 @@
-category_lookup = 'category'
-product_lookup = 'product'
-tocart_lookup = 'tocart'
-fromcart_lookup = 'fromcart'
-separator = '_'
--- webshop/main.py
+++ webshop/main.py
@@ -1,13 +1,13 @@
 from webshop.bot.main import *
 from flask import Flask, request, abort
 from telebot.types import Update
-from webshop.bot.config import WEBHOOK_PATH, WEBHOOK_URL
+from webshop.bot import config
 
 
 app = Flask(__name__)
 
 
-@app.route(WEBHOOK_PATH, methods=['POST'])
+@app.route(config.WEBHOOK_PATH, methods=['POST'])
 def webhook():
     if request.headers.get('content-type') == 'application/json':
         json_string = request.get_data().decode('utf-8')
@@ -20,7 +20,7 @@ def webhook():
 
 if __name__ == '__main__':
     bot.set_webhook(
-        WEBHOOK_URL,
+        config.WEBHOOK_URL,
         certificate=open('webhook_cert.pem', 'r')
     )
     app.run(debug=True)
